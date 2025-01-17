14a7daba1ab1e28c00e13b59e931d63840f9fe87
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-17 11:31:44 -0300

    feat: add new button size prop

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,9 +1,9 @@
-@props(['variant' => 'primary', 'link' => false])
+@props(['variant' => 'primary', 'link' => false, 'size' => 'base'])
 
 @php
 
     $baseClasses =
-        'inline-flex items-center px-4 py-2 rounded-md font-semibold text-xs  uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150';
+        'inline-flex items-center font-semibold uppercase focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150';
 
     $variants = [
         'primary' =>
@@ -14,7 +14,12 @@
             'border border-gray-800 hover:border-gray-600 focus:border-gray-600 active:border-gray-900 focus:ring-indigo-500 text-gray-800',
     ];
 
-    $classes = $baseClasses . ' ' . $variants[$variant];
+    $sizes = [
+        'small' => 'rounded-md px-2 py-1.5 text-xs',
+        'base' => 'rounded-md px-4 py-2 text-xs tracking-widest',
+    ];
+
+    $classes = $baseClasses . ' ' . $variants[$variant] . ' ' . $sizes[$size];
 @endphp
 
 @if ($link === true)
