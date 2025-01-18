28ff1dea6e09a8b471c2a7f172254524e6958d17
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-18 15:41:38 -0300

    feat: add tag factory and seeder

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
+        'user_id',
         'name',
         'slug'
     ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProjectSeeder::class,
-            TaskSeeder::class
+            TaskSeeder::class,
+            TagSeeder::class
         ]);
     }
 }
