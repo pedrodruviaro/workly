f4a025ed709bdfc4694330baa98267af97438eb1
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-19 11:02:24 -0300

    refactor: select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,8 +1,8 @@
-@props(['options'])
+@props(['options', 'value'])
 
 <select
     {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm resize-none']) }}>
-    @foreach ($options as $key => $value)
-        <option value="{{ $key }}">{{ $value }}</option>
+    @foreach ($options as $key => $val)
+        <option value="{{ $key }}" {{ $key == $value ? 'selected' : '' }}>{{ $val }}</option>
     @endforeach
 </select>
